ce9c5db5360e6ef9a6a381f875de2bebb68d6d67
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2025-01-09 11:35:29 +0200

    add: shadcn/ui, form, button, input components, sonner toaster

--- next-auth/src/app/(public)/login/page.tsx
+++ next-auth/src/app/(public)/login/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import LoginForm from "@/components/login-form";
 
 export default function Home() {
   return (
     <main>
       <div className="container">
-        <Button variant='destructive'>Hekko World</Button>
+        <LoginForm />
       </div>
     </main>
   );
